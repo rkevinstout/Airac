266f9ccf1fb073f3d4ec5ea165e6522900635b8c
Kevin Stout <rkevinstout@gmail.com>
2023-08-19 15:08:07 -0500

    test: fix formating, data typos

--- tests/Airac.Tests/CycleTests.cs
+++ tests/Airac.Tests/CycleTests.cs
@@ -20,7 +20,7 @@ public class CycleTests
     [Fact]
     public void SomeYearsHaveFourteenCycles()
     {
-        var date = new DateOnly(2021, 1, 2);
+        var date = new DateOnly(2020, 12, 31);
 
         var cycle = new Cycle(date);
 
@@ -31,11 +31,10 @@ public class CycleTests
     [MemberData(nameof(AirNavData))]
     public void EffectiveDateAndOrdinalsAreCorrect(int ordinal, DateOnly effectiveDate)
     {
-        var cycle = new Cycle(effectiveDate.AddDays(1));
+        var cycle = new Cycle(effectiveDate.AddDays(27));
 
-        cycle.EffectiveDate.Should().Be(effectiveDate);
         cycle.Ordinal.Should().Be(ordinal);
-
+        cycle.EffectiveDate.Should().Be(effectiveDate);
     }
 
     /// <summary>
@@ -44,31 +43,31 @@ public class CycleTests
     /// </summary>
     public static TheoryData<int, DateOnly> AirNavData => new()
         {
-            {1, new DateOnly(2023, 1, 26) },
-            {2, new DateOnly(2023, 2, 23) },
-            {3, new DateOnly(2023, 3, 23) },
-            {4, new DateOnly(2023, 4, 20) },
-            {5, new DateOnly(2023, 5, 10) },
-            {6, new DateOnly(2023, 6, 15) },
-            {7, new DateOnly(2023, 7, 13) },
-            {8, new DateOnly(2023, 8, 10) },
-            {9, new DateOnly(2023, 9, 7) },
-            {10, new DateOnly(2023, 10, 5) },
-            {11, new DateOnly(2023, 11, 2) },
-            {12, new DateOnly(2023, 11, 30) },
-            {13, new DateOnly(2023, 12, 28) },
-            {1, new DateOnly(2024, 1, 25) },
-            {2, new DateOnly(2024, 2, 22) },
-            {3, new DateOnly(2024, 3, 21) },
-            {4, new DateOnly(2024, 4, 18) },
-            {5, new DateOnly(2024, 5, 16) },
-            {6, new DateOnly(2024, 6, 13) },
-            {7, new DateOnly(2024, 7, 11) },
-            {8, new DateOnly(2024, 8, 8) },
-            {9, new DateOnly(2024, 9, 5) },
-            {10, new DateOnly(2024, 10, 3) },
-            {11, new DateOnly(2024, 10, 31) },
-            {12, new DateOnly(2024, 11, 28) },
-            {13, new DateOnly(2024, 12, 26) },
+            {  1, new DateOnly(2023, 1, 26) },
+            {  2, new DateOnly(2023, 2, 23) },
+            {  3, new DateOnly(2023, 3, 23) },
+            {  4, new DateOnly(2023, 4, 20) },
+            {  5, new DateOnly(2023, 5, 18) },
+            {  6, new DateOnly(2023, 6, 15) },
+            {  7, new DateOnly(2023, 7, 13) },
+            {  8, new DateOnly(2023, 8, 10) },
+            {  9, new DateOnly(2023, 9, 7) },
+            { 10, new DateOnly(2023, 10, 5) },
+            { 11, new DateOnly(2023, 11, 2) },
+            { 12, new DateOnly(2023, 11, 30) },
+            { 13, new DateOnly(2023, 12, 28) },
+            {  1, new DateOnly(2024, 1, 25) },
+            {  2, new DateOnly(2024, 2, 22) },
+            {  3, new DateOnly(2024, 3, 21) },
+            {  4, new DateOnly(2024, 4, 18) },
+            {  5, new DateOnly(2024, 5, 16) },
+            {  6, new DateOnly(2024, 6, 13) },
+            {  7, new DateOnly(2024, 7, 11) },
+            {  8, new DateOnly(2024, 8, 8) },
+            {  9, new DateOnly(2024, 9, 5) },
+            { 10, new DateOnly(2024, 10, 3) },
+            { 11, new DateOnly(2024, 10, 31) },
+            { 12, new DateOnly(2024, 11, 28) },
+            { 13, new DateOnly(2024, 12, 26) },
         };
 }
